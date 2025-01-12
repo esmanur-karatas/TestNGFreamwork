c40d27e85a2454594d30277f4859b40acefc96cf
Esmanur <esmanurkaratas0@gmail.com>
2025-01-12 22:13:20 +0300

    update

--- src/test/java/com/TestNgFreamwork/utilities/ReusableMothods.java
+++ src/test/java/com/TestNgFreamwork/utilities/ReusableMothods.java
@@ -2,6 +2,7 @@ package com.TestNgFreamwork.utilities;
 
 public class ReusableMothods {
 
+    //burada sıklıkla kullanacağımız methodları yazabiliriz mesele çerezleri kabul et thread vs gibi.
     public static void sleep(int miliseconds) throws InterruptedException {
         try {
             Thread.sleep(miliseconds);
